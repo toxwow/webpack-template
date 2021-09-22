0d339b70ac58db1517adb51ceec72d711ac61f9b
Tomasz Tomżyński <tomasztomzynski@MacBook-Pro-Tomasz.local>
2021-09-22 15:04:13 +0200

    init Webpack Template

---
+++ src/js/app.js
@@ -0,0 +1,14 @@
+import $ from 'jquery';
+import 'bootstrap';
+
+// plugins
+import '@fortawesome/fontawesome-free';
+
+// modules
+import './module/main-menu';
+
+// views
+import './views/homepage';
+
+window.$ = $;
+window.jQuery = $;
---
+++ webpack/config.prod.babel.js
@@ -0,0 +1,119 @@
+const fs = require('fs');
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const htmlFileNames = fs.readdirSync('./src/html/');
+
+const getEntries = () => {
+    const entries = [
+        './src/js/app.js',
+        './src/scss/app.scss'
+    ];
+
+    htmlFileNames.forEach(filename => {
+        entries.push(`./src/html/${filename}`);
+    });
+
+    return entries;
+};
+
+const getPlugins = () => {
+    const plugins = [
+        new webpack.ProvidePlugin({
+            '$': "jquery",
+            'jQuery': "jquery",
+            'window.jQuery': 'jquery',
+            'Popper': 'popper.js',
+            "Bootstrap": "bootstrap.js"
+        }),
+        new CleanWebpackPlugin('dist', {
+            root: __dirname + '/../'
+        }),
+        new CopyWebpackPlugin([
+            {
+                from: __dirname + '/../src/assets/',
+                to: __dirname + '/../public_html/assets/'
+            }
+        ]),
+        new ExtractTextPlugin({
+            filename: './assets/css/styles.css',
+            allChunks: true
+        })
+    ];
+    htmlFileNames.forEach(filename => {
+        if (filename.substr(0, 1) !== '_') {
+            const splitted = filename.split('.');
+            if (splitted[1] === 'html') {
+                plugins.push(
+                    new HtmlWebpackPlugin({
+                        template: `./src/html/${filename}`,
+                        filename: `./${filename}`,
+                        path: __dirname + '/../public_html'
+                    }),
+                );
+            }
+        }
+    });
+
+    return plugins;
+};
+
+module.exports = {
+    entry: getEntries(),
+    output: {
+        filename: './assets/js/bundle.js',
+        path: __dirname + '/../public_html'
+    },
+    plugins: getPlugins(),
+    module: {
+        rules: [
+            {
+                test: /\.(html)$/,
+                loader: path.resolve(__dirname, 'loader/html-loader.js'),
+                options: {
+                    html: htmlFileNames
+                }
+            },
+            {
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+            {
+                test: /\.(css|scss)$/,
+                exclude: /node_modules/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        {
+                            loader: "css-loader",
+                            options: {
+
+                                url: false
+                            }
+                        }, {
+                            loader: "postcss-loader",
+                            options: {
+                                ident: 'postcss',
+                                plugins: () => [
+                                    require('autoprefixer')({
+                                        browsers: ['ie >= 8', 'last 4 version']
+                                    }),
+                                    require('cssnano')()
+                                ]
+                            }
+                        },
+                        'sass-loader'
+                    ]
+                })
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.jpg', '.html', '.scss'],
+    }
+};
